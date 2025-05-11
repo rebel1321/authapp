bf64f38885a31d9849fa7c22a89b98e2ddd672fb
Satyam <www.satyam200413@gmail.com>
2025-05-11 11:23:45 +0530

    Made signup handle function in signup ui later I'll add its ui

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { toast } from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
+export default function SignupPage() {
+  const router = useRouter()
+  const [user,setUser] = useState({
+    email:"",
+    password:"",
+    username:""
+  })
+  const [ buttonDisabled,setButtonDisabled] =useState(false)
 
-export default function page() {
+  const [loading,setLoading]=useState(false)
+
+  const onSignup = async()=>{
+    try {
+      setLoading(true)
+      const response = await axios.post('/api/users/signup',user)
+      console.log("Signup Success",response.data)
+      router.push('/login')
+    } catch (error:any) {
+      console.log("Signup failed")
+      toast.error(error.message)
+    }
+  }
+  useEffect(()=>{
+    if(user.email.length>0 && user.password.length>0 && user.username.length>0){
+      setButtonDisabled(false)
+    }else{
+        setButtonDisabled(true)
+
+    }
+  },[user])
   return (
-    <div>page</div>
+    <div>page</div> 
   )
 }
+// !8 min 26
